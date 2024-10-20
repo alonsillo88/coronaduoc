4bdb7f436a40514a49547694d544433e6c5fd110
Alonso González <alonso.gonzale05@gmail.com>
2024-10-19 22:07:19 -0300

    Nueva versión para Azure configurada con puerto 8080

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   app.enableShutdownHooks();
   app.useGlobalPipes(new ValidationPipe());
   
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(8080, '0.0.0.0');
 }
 
 bootstrap();
